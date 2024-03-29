511679e13d0d7a163b2686cce2e56a847419cdf5
florian2881 <165366976+florian2881@users.noreply.github.com>
2024-03-29 10:46:18 +0000

    GUI V0

--- gui/gui.py
+++ gui/gui.py
@@ -1,11 +1,22 @@
 import tkinter as tk
-from utils.board import SudokuBoard
+# from utils.board import SudokuBoard
 
 class SudokuGUI:
     def __init__(self):
         self.window = tk.Tk()
-        self.board = SudokuBoard()
-        # Créez la grille de Sudoku et les autres éléments de l'interface graphique.
+        # self.board = SudokuBoard()
+
+        self.window.title("Sudoku")
+        self.window.config(background='White')
+        self.window.geometry("1920x1080")
+        self.window.minsize(1920,1080)
+        self.window.maxsize(1920,1080)
+
+        titre = Label(self.window, text="SUDOKU", font="Calibri, 40", fg='Black')
+        titre.pack(side=TOP)
 
     def run(self):
         self.window.mainloop()
+
+gui = SudokuGUI()
+gui.run()
