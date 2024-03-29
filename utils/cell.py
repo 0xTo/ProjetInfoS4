5cd4bfe952d023e34f9c29a6cd516e4799ed8b16
theom-e <admin@theom-e.fr>
2024-03-29 11:10:15 +0100

    init cell class

--- utils/cell.py
+++ utils/cell.py
@@ -2,3 +2,16 @@ class SudokuCell:
     def __init__(self, value=0):
         self.value = value
         self.candidates = set(range(1, 10)) if value == 0 else set()
+
+        def __str__(self):
+            return str(self.value)
+
+        def changeValue(self, value):
+            self.value = value
+            self.candidates = set()
+
+        def removeCandidate(self, value):
+            self.candidates.remove(value)
+
+        def addCandidate(self, value):
+            self.candidates.add(value)
