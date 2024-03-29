ae36774d5b26653417914fc05d858c7c423ea4e5
theom-e <admin@theom-e.fr>
2024-03-29 11:07:11 +0100

    created getSquare

--- utils/board.py
+++ utils/board.py
@@ -1,17 +1,25 @@
 from cell import SudokuCell
 
+
 class SudokuBoard:
     def __init__(self):
         self.grid = [[SudokuCell() for _ in range(9)] for _ in range(9)]
-        
-    def getColsBoard(self,cols: int)->list:
+
+    def getColsBoard(self, cols: int) -> list:
         colonne = []
         for i in range(9):
-            colonne.append(self.grid[i][cols])
+            colonne.append(self.grid[i][cols].value)
         return colonne
-    def getRowBoard(self, row: int)->list:
+
+    def getRowBoard(self, row: int) -> list:
         ligne = []
         for i in range(9):
-            ligne.append(self.grid[row][i])
+            ligne.append(self.grid[row][i].value)
         return ligne
 
+    def getSquareBoard(self, row: int, cols: int) -> list:
+        square = []
+        for i in range(3):
+            for j in range(3):
+                square.append(self.grid[i + row][j + cols].value)
+        return square
