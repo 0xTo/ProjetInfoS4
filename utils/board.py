e921a80ff8851492330daaf7574c1812971bf38d
Joseph <67434468+Illigo19@users.noreply.github.com>
2024-03-29 09:52:13 +0000

    ADD getColsBoard(), getRowBoard

--- utils/board.py
+++ utils/board.py
@@ -3,3 +3,15 @@ from cell import SudokuCell
 class SudokuBoard:
     def __init__(self):
         self.grid = [[SudokuCell() for _ in range(9)] for _ in range(9)]
+        
+    def getColsBoard(self,cols: int)->list:
+        colonne = []
+        for i in range(9):
+            colonne.append(self.grid[i][cols])
+        return colonne
+    def getRowBoard(self, row: int)->list:
+        ligne = []
+        for i in range(9):
+            ligne.append(self.grid[row][i])
+        return ligne
+
